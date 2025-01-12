664e14f80a658911c449bfb793d6b892d03fb295
Asunaro276 <ryuhei276276@gmail.com>
2025-01-12 12:36:33 +0900

    [update] iamを修正

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -50,6 +50,6 @@ resource "aws_iam_policy" "amplify_iam_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "amplify_role_policy_attachment" {
-  role       = aws_iam_role.amplify_iam_role.arn
+  role       = aws_iam_role.amplify_iam_role.name
   policy_arn = aws_iam_policy.amplify_iam_policy.arn
 }
