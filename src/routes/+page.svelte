bfb9d5c0dd9fed3f2bb4a1a5c7e4ab4d078f4b92
Asunaro276 <ryuhei276276@gmail.com>
2025-01-12 11:15:16 +0900

    [update] エラーが出ないように修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <h1>Welcome to SvelteKit</h1>
 <div>
-  <label>email</label>
-  <input type="email" />
+  <label for="email">email</label>
+  <input type="email" id="email" />
 </div>
 <div>
-  <label>password</label>
-  <input type="password" />
+  <label for="password">password</label>
+  <input type="password" id=password />
 </div>
-  flex-direction: column;
-}
+
+<style lang="scss">
 input {
   max-width: 200px;
 }
